ec48e5881599f53fd852eadd78d4c6078d626ecc
Igor Nadworski <inadworski@gmail.com>
2019-08-28 08:50:27 +0200

    eslint, prettier, konva

---
+++ src/components/sidePanel/SidePanelComponent.jsx
@@ -0,0 +1,11 @@
+import React, { Component, Fragment } from "react";
+
+export default class SidePanel extends Component {
+  state = {
+    herebenothing: false
+  };
+
+  render() {
+    return "test";
+  }
+}
